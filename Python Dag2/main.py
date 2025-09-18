f48807fc297992bfb0e9120f6b45a5dcbcb4c6e7
jimmibram <post@jimmibram.dk>
2025-09-18 09:20:28 +0200

    Opdaterer dag 2

--- Python Dag2/main.py
+++ Python Dag2/main.py
@@ -1,1 +1,27 @@
-print("Velkommen til dag 2!")
+# Her kommer der noget om variabler
+
+#int = heltal
+alder = 25
+print("Min alder er", alder)
+
+#Float = kommatal
+pengePaakontoen = 22.5
+pengePaakontoen = pengePaakontoen + 1.5
+print("Jeg har",pengePaakontoen, "kr på min konto.")
+
+#string / streng / tekst
+navn = "Jimmi"
+
+print("Mit navn er", navn)
+
+#bool / bit
+iskole = True
+print("Er jeg i skole?", iskole)
+
+#input
+navn = input("Hvad hedder du?")
+print(navn, "du er cool!")
+
+
+print("-------------------------------------")
+print("| Hej")
