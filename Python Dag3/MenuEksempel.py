e758fc75346bb63dc3ffbfda7a417d68c2b6f1e2
jimmibram <post@jimmibram.dk>
2025-10-02 11:07:48 +0200

    Menu

---
+++ Python Dag3/MenuEksempel.py
@@ -0,0 +1,23 @@
+import atm_funktioner
+
+
+while 1==1:
+    print("VELKOMMEN TIL MENUEN")
+    print("----------------------")
+    print("Hvad vil du spise?")
+    print("  1. Ost")
+    print("  2. Skinke")
+    print("  3. Salat")
+    print("  4. EXIT")
+
+    valg = input("")
+    
+    print("Du valgte", valg)
+    
+    if(valg == "1"):
+        print("OST ER OGSÅ DEJLIGT!")
+    elif(valg == "4"):
+        break
+
+print("Farveller")
+    
